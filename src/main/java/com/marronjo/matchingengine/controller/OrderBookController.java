d2c5c66eca1f9653b4f01960982821a9dda7ea4e
Joe Marron <marronjo@tcd.ie>
2023-07-08 22:57:10 +0100

    add details to swagger docs

--- src/main/java/com/marronjo/matchingengine/controller/OrderBookController.java
+++ src/main/java/com/marronjo/matchingengine/controller/OrderBookController.java
@@ -3,19 +3,23 @@ package com.marronjo.matchingengine.controller;
 import com.marronjo.matchingengine.domain.orders.request.OrderRequest;
 import com.marronjo.matchingengine.domain.orders.response.OrderResponse;
 import com.marronjo.matchingengine.service.OrderBookService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static com.marronjo.matchingengine.domain.constant.Constants.*;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/orderbook")
+@RequestMapping(ORDERBOOK_URL)
 public class OrderBookController {
 
     private final OrderBookService orderBookService;
 
-    @PostMapping("send-order/{ticker}")
+    @PostMapping(SEND_ORDER_URL)
+    @ApiOperation(value = SEND_ORDER_VALUE, notes = SEND_ORDER_NOTES)
     public ResponseEntity<OrderResponse> sendOrder(@PathVariable String ticker, @RequestBody OrderRequest order){
         return ResponseEntity.status(HttpStatus.OK).body(orderBookService.submitOrder(ticker, order));
     }
---
+++ src/main/java/com/marronjo/matchingengine/domain/constant/Constants.java
@@ -0,0 +1,11 @@
+package com.marronjo.matchingengine.domain.constant;
+
+public class Constants {
+    public static final String ORDERBOOK_URL = "/api/orderbook";
+    public static final String SEND_ORDER_URL = "/send-order/{ticker}";
+    public static final String SEND_ORDER_VALUE = "Endpoints to facilitate orderbook interaction";
+    public static final String SEND_ORDER_NOTES =
+            """
+            
+            """;
+}
