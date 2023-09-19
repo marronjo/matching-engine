a9322bee019b6e9f60af8cc9fa05c157a51d9e74
Joe Marron <68810618+marronjo@users.noreply.github.com>
2023-09-19 21:57:40 +0100

    normalise API url

--- src/main/java/com/marronjo/matchingengine/domain/constant/Constants.java
+++ src/main/java/com/marronjo/matchingengine/domain/constant/Constants.java
@@ -1,8 +1,8 @@
 package com.marronjo.matchingengine.domain.constant;
 
 public class Constants {
-    public static final String ORDERBOOK_URL = "/api/orderbook";
-    public static final String SEND_ORDER_URL = "/send-order/{ticker}";
+    public static final String ORDERBOOK_URL = "/api/orders";
+    public static final String SEND_ORDER_URL = "/api/orders/{ticker}";
     public static final String SEND_ORDER_VALUE = "Endpoints to facilitate orderbook interaction";
     public static final String SEND_ORDER_NOTES =
             """
